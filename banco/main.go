7530369b58afd3484415121c1753c888be616d0d
Francini Silva <francinisilva89@hotmail.com>
2023-05-17 00:46:26 -0300

    Interface  e novo tipo de conta

--- banco/main.go
+++ banco/main.go
@@ -5,11 +5,25 @@ import (
 	"poo/banco/contas"
 )
 
+func PagarVoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 
-	contaExemplo := contas.ContaCorrente{}
-	contaExemplo.Depositar(100)
+	contaDoDenis := contas.ContaPoupanca{}
+	contaDoDenis.Depositar(100)
+	PagarVoleto(&contaDoDenis, 60)
 
-	fmt.Println(contaExemplo.ObterSaldo())
+	fmt.Println(contaDoDenis.ObterSaldo())
+
+	contaDaPati := contas.ContaCorrente{}
+	contaDaPati.Depositar(600)
+	PagarVoleto(&contaDaPati, 60)
+	fmt.Println(contaDaPati.ObterSaldo())
 
 }
