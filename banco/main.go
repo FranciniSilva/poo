e600a9ee6202491dc06e8c8d48c9370a9b31c92c
Francini Silva <francinisilva89@hotmail.com>
2023-05-16 22:26:50 -0300

    Comparando tipos

--- banco/main.go
+++ banco/main.go
@@ -12,12 +12,25 @@ type ContaCorrente struct {
 func main() {
 
 	contaDaFrancini := ContaCorrente{titular: "Francini", numeroAgencia: 859, numeroConta: 123456, saldo: 500}
-	contaDaCarol := ContaCorrente{titular: "Carol", numeroAgencia: 859, numeroConta: 111222, saldo: 5080}
-	fmt.Println(contaDaFrancini, contaDaCarol)
+	contaDaFrancini2 := ContaCorrente{titular: "Francini", numeroAgencia: 859, numeroConta: 123456, saldo: 500}
+	// contaDaCarol := ContaCorrente{titular: "Carol", numeroAgencia: 859, numeroConta: 111222, saldo: 5080}
+	// contaDaCarol2 := ContaCorrente{titular: "Carol", numeroAgencia: 859, numeroConta: 111222, saldo: 5080}
+	fmt.Println(contaDaFrancini == contaDaFrancini2)
 
 	var contaDaCris *ContaCorrente
 	contaDaCris = new(ContaCorrente)
 	contaDaCris.titular = "Cris"
+	contaDaCris.saldo = 500
 
-	fmt.Println(*contaDaCris)
+	// fmt.Println(*contaDaCris)
+
+	var contaDaCris2 *ContaCorrente
+	contaDaCris = new(ContaCorrente)
+	contaDaCris.titular = "Cris"
+	contaDaCris.saldo = 500
+
+	fmt.Println(contaDaCris == contaDaCris2)
+	fmt.Println(&contaDaCris)
+	fmt.Println(&contaDaCris2)
+	fmt.Println(*contaDaCris == *contaDaCris2)
 }
